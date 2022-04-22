8bbaa9df852991be11c72888ba6ba01b6abf61a8
dell <romkacarry@gmail.com>
2022-04-22 10:45:30 +0300

    Add utils directory

--- components/navbar/list-item.js
+++ components/navbar/list-item.js
@@ -1,9 +1,13 @@
-const  = () => {
+import Link from 'next/link';
+
+const ListItem = ({ href, linkContent }) => {
   return (
-  <>
+    <li>
+      <Link href={href}>
+        <a>{linkContent}</a>
+      </Link>
+    </li>
+  );
+};
 
-  </>
-   )
-  }
-
-export default Ite
+export default ListItem;
---
+++ utils/constants.js
@@ -0,0 +1,8 @@
+import Link from 'next/link';
+
+export const pages = {
+  home: { route: '/', linkContent: 'Home' },
+  about: { route: '/about', linkContent: 'About' },
+  new: { route: '/new', linkContent: 'New' },
+  contact: { route: '/contact', linkContent: 'Contact Us' },
+};
