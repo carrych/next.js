a2cb4ae355f1c5b22f7aa6e8f9f6618217bc78a7
dell <romkacarry@gmail.com>
2022-04-26 22:52:53 +0300

    Add styles for user

--- pages/new/index.js
+++ pages/new/index.js
@@ -1,7 +1,24 @@
-const New = () => {
+import styles from '../../styles/Users.module.css';
+
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+export const getStaticProps = async () => {
+  const res = await fetch(url);
+  const users = await res.json();
+
+  return { props: { users } };
+};
+
+const New = ({ users }) => {
   return (
-    <div className="new">
-      <h1>Hey ho THIS IS NEW!</h1>
+    <div className={styles.list}>
+      <ul>
+        {users.map((user) => (
+          <li className={styles.single} key={`${user.id}${user.name}`}>
+            {user.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
