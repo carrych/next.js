42060e8f500ca169af8223c7bc8c20143ad243b0
dell <romkacarry@gmail.com>
2022-04-26 10:06:40 +0300

    Refactor styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello!!!</h1>
+  return (
+    <div className={styles.container}>
+      {' '}
+      <h1 className={styles.title}>Hello!!!</h1>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores commodi
+        dignissimos doloremque ea iure iusto nobis placeat quaerat quia? Aspernatur deleniti
+        doloribus ducimus ipsam magni, maiores modi provident repellendus.
+      </p>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores commodi
+        dignissimos doloremque ea iure iusto nobis placeat quaerat quia? Aspernatur deleniti
+        doloribus ducimus ipsam magni, maiores modi provident repellendus.
+      </p>
+    </div>
+  );
 }
